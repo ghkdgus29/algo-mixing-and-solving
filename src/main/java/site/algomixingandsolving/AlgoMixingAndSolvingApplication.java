b3f7d22ca6b8563984cb7a9ed70b08a14cccd361
Hyun <ghkdgus29@naver.com>
2024-07-03 18:50:33 +0900

    init: 프로젝트 설정

---
+++ src/main/java/site/algomixingandsolving/AlgoMixingAndSolvingApplication.java
@@ -0,0 +1,13 @@
+package site.algomixingandsolving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoMixingAndSolvingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoMixingAndSolvingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/algomixingandsolving/AlgoMixingAndSolvingApplicationTests.java
@@ -0,0 +1,13 @@
+package site.algomixingandsolving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoMixingAndSolvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
